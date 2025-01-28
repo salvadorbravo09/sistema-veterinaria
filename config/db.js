9d78a804159c92416ea2304f178bcd72ed9b25fc
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-27 23:05:19 -0300

    feat: import envs and use variable environment

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect(
-      "mongodb+srv://sbravoc03:4DkXfryUZZb9zPZK@cluster0.ak3sg.mongodb.net/",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     const url = `${db.connection.host}:${db.connection.port}`;
     console.log(`MongoDB connect on ${url}`);
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+// Crea una instancia de la aplicación Express
 const app = express();
 
+// Configura dotenv para cargar las variables de entorno
+dotenv.config();
+
+// Llama a la función connectDB para establecer una conexión con la base de datos
 connectDB();
 
+// Inicia el servidor en el puerto 4000 y muestra un mensaje en la consola
+const port = process.env.PORT || 4000;
 app.listen(4000, () => {
-  console.log("Server running on port 4000");
+  console.log(`Server running on port ${port}`);
 });
