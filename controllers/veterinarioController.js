156b516deabd7ec00353e3353d18ab89cf311865
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-30 17:41:18 -0300

    feat: save data in db

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,9 +1,18 @@
-const registrar = (req, res) => {
-  res.send("Desde API/VETERINARIOS");
+import veterinarioModel from "../models/veterinarioModel.js";
+
+const registrar = async (req, res) => {
+  try {
+    // Guardar un nuevo veterinario en la base de datos
+    const veterinario = new veterinarioModel(req.body);
+    const veterinarioGuardado = await veterinario.save();
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const perfil = (req, res) => {
-  res.send("Desde API/VETERINARIOS/perfil");
+  res.json({ msg: "Mostrando perfil" });
 };
 
 export { registrar, perfil };
