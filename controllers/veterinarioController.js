d7d384531ef76bd4dbe1f35a409d271857c32f94
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-30 18:51:43 -0300

    feat: prevent duplicate user registrations

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,6 +1,14 @@
 import veterinarioModel from "../models/veterinarioModel.js";
 
 const registrar = async (req, res) => {
+  const { email } = req.body;
+  const emailExiste = await veterinarioModel.findOne({ email });
+
+  if (emailExiste) {
+    const error = new Error("El email ya esta registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+
   try {
     // Guardar un nuevo veterinario en la base de datos
     const veterinario = new veterinarioModel(req.body);
