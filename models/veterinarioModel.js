7cd341ce94e045ae680c35b3e926ce4272832b6b
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-01-31 15:35:17 -0300

    fix: fix

--- models/veterinarioModel.js
+++ models/veterinarioModel.js
@@ -41,6 +41,7 @@ veterinarioSchema.pre("save", async function () {
   if (!this.isModified("password")) {
     next();
   }
+  
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
