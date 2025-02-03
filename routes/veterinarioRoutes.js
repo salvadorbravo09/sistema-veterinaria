a6a87d3f879072b7e8039fcbc58c66618749d68b
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-02-03 19:18:17 -0300

    feat: create routes for reset password

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -4,6 +4,9 @@ import {
   perfil,
   confirmar,
   autenticar,
+  resetPassword,
+  comprobarToken,
+  nuevoPassword,
 } from "../controllers/veterinarioController.js";
 import checkAuth from "../middlewares/authMiddleware.js";
 
@@ -13,6 +16,9 @@ const router = express.Router();
 router.post("/", registrar); // Ruta para registrar un nuevo veterinario
 router.post("/login", autenticar); // Ruta para autenticar un veterinario
 router.get("/confirmar/:token", confirmar); // Ruta para confirmar la cuenta de un veterinario
+router.post("/resetear-password", resetPassword); // Ruta para resetear la contraseña de un veterinario
+router.get("/resetear-password/:token", comprobarToken); // Ruta para comprobar el token de reseteo de contraseña
+router.post("/resetear-password/:token", nuevoPassword); // Ruta para cambiar la contraseña de un veterinario
 
 // Private Routes
 router.get("/perfil", checkAuth, perfil); // Ruta para ir al perfil de un veterinario
