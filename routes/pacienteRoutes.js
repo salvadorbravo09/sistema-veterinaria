dd1f98cba97f41bd592896474825ce05f4560537
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-02-04 18:56:18 -0300

    feat: implement auth middleware in patient routes

--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -3,10 +3,11 @@ import {
   agregarPaciente,
   obtenerPacientes,
 } from "../controllers/pacienteController.js";
+import checkAuth from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", agregarPaciente);
+router.post("/", checkAuth, agregarPaciente);
 router.get("/", obtenerPacientes);
 
 export default router;
