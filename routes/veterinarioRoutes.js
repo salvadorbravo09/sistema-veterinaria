06e23f5b051b02f2834cc328914f705147866b15
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2025-02-09 21:52:10 -0300

    docs: create documentation for confirm account

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -87,7 +87,29 @@ router.post("/", registrar); // Ruta para registrar un nuevo veterinario
  */
 router.post("/login", autenticar); // Ruta para autenticar un veterinario
 
+/**
+ * @swagger
+ * /api/veterinarios/confirmar/{token}:
+ *   get:
+ *     summary: Confirmar cuenta de veterinario
+ *     description: Confirma la cuenta de un veterinario usando un token.
+ *     tags: [Veterinarios]
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Token de confirmación
+ *     responses:
+ *       200:
+ *         description: Cuenta confirmada con éxito.
+ *       404:
+ *         description: Token inválido.
+ */
 router.get("/confirmar/:token", confirmar); // Ruta para confirmar la cuenta de un veterinario
+
+
 router.post("/resetear-password", resetPassword); // Ruta para resetear la contraseña de un veterinario
 router.get("/resetear-password/:token", comprobarToken); // Ruta para comprobar el token de reseteo de contraseña
 router.post("/resetear-password/:token", nuevoPassword); // Ruta para cambiar la contraseña de un veterinario
